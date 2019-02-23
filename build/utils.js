6e422121623b87c8bc36dec7f92f1f13c885e642
linshenghao <1148780544@qq.com>
2019-02-23 08:56:59 +0800

    注释移动端全局rem适配

--- build/utils.js
+++ build/utils.js
@@ -62,7 +62,7 @@ exports.cssLoaders = function (options) {
 
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    const loaders = options.usePostCSS ? [cssLoader,postcssLoader,px2remLoader] : [cssLoader,px2remLoader] //px2remLoader  px2remLoader
+    const loaders = options.usePostCSS ? [cssLoader,postcssLoader] : [cssLoader] //px2remLoader  px2remLoader
 
     if (loader) {
       loaders.push({
